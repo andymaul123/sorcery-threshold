24bc0fdc02eff322d94dbc05bbae18dae0d788b3
Andrew Maul <andymaul123@gmail.com>
2025-06-28 23:20:17 -0700

    Refactors generate-combinations, adds comments explaining strategy

--- generate-combinations.mjs
+++ generate-combinations.mjs
@@ -1,6 +1,48 @@
 import { createFrequencyMap } from "./utils.mjs";
 
 /**
+ * A truncated explanation of the strategy behind generateCombinations.
+ * 
+ * Given criteria [a,e], a site deck of [a,af,e,w] contains two possible combinations that include the criteria.
+ * 
+ * Next a combinationsArray is created, and seeded with the criteria as its first item.
+ * combinationsArray = [[a,e]]
+ * 
+ * The script will loop over every item in the combinationsArray (1 item to start), but also pushes new combinations it finds into the combinationsArray
+ * as the loop progresses.
+ * 
+ * Starting at the first item, [a,e], another loop is performed over each symbol. The first symbol is 'a'.
+ * 
+ * A *third* loop occurs, but this time over every item in the site deck. The symbol being looped over ('a') is compared with the current symbol in
+ * the site deck.
+ * If the symbol in the site deck would make a 'valid combination', the symbol is replaced with the one from the site deck and that new combination is pushed to the 
+ * combinationsArray.
+ * 
+ * 'a' is being replaced, check 'a'. Skip, as it's the exact symbol we're checking for.
+ * 'a' is being replaced, check 'af'. Match. Combination [af,e] will be pushed to combinationsArray if not already in there.
+ * 'a' is being replaced, check 'e'. [e,e] is not a match.
+ * 'a' is being replaced, check 'w'. [w,e] is not a match.
+ * 
+ * Now do the next symbol in [a,e]:
+ * 'e' is being replaced, check 'a'. [a,a] is not a match.
+ * 'e' is being replaced, check 'af'. [a,af] is not a match.
+ * 'e' is being replaced, check 'e'. Skip, as it's the exact symbol we're checking for.
+ * 'e' is being replaced, check 'w'. [a,w] is not a match.
+ * 
+ * Finished with [a,e], move onto the next item in the combinationsArray, which is [af,e]. No new combinations are found, thus the final combinationsArray
+ * is [[a,e],[af,e]].
+ * 
+ * This strategy works nicely when you are trying to find possible combinations in the same number of draws as the criteria length, i.e.
+ * "find [a,e] in two draws." When additional draws are added, things get more complicated.
+ * 
+ * When there is a 'drawCount', the criteria array is filled up with wildcard (*) characters to match the drawCount length. Using the 
+ * above example, criteria [a,e] with drawCount 3 creates a new criteria of [a,e,*].
+ * 
+ * The same overall approach is taken, except wild cards get replaced with any symbol, and additional logic is needed in areas to account for them.
+ */
+
+
+/**
  * Given an array of values and a criteria, generates all possible combinations from the array that satisfy the criteria
  * @param {Array<string>} initialCriteria
  * @param {Array<string>} siteDeckSymbols
@@ -19,11 +61,7 @@ export function generateCombinations(initialCriteria, siteDeckSymbols, drawCount
             criteria.push(MATCH_ANY);
         }
     }
-
-    const criteriaFrequencyMap = createFrequencyMap(criteria);
-    const reducedSiteDeckSymbols = reduceSiteDeckSymbols(criteria, criteriaFrequencyMap, siteDeckSymbols);
     const decklistFrequencyMap = createFrequencyMap(siteDeckSymbols);
-    const reducedDecklistFrequencyMap = createFrequencyMap(reducedSiteDeckSymbols);
     
     let matchCount = 0;
     const combinationsArray = [];
@@ -53,17 +91,15 @@ export function generateCombinations(initialCriteria, siteDeckSymbols, drawCount
         // For each value in a given combinationsArray item array, e.g. [a,e,e,w]
         for (let selectedCombinationIndex = 0; selectedCombinationIndex < combinationsArray[combinationsArrayIndex].length; selectedCombinationIndex++) {
             // we're now nested down to the individual letter, e.g. "a" or "e"
-            // For each item in reducedSiteDeckSymbols or siteDeckSymbols array, e.g. 'a' or 'aef'
-            // Comparing individual symbols from the current combinations array against the symbol in the criteria, 'a' <-> 'aef'
+            // For each item in siteDeckSymbols array, e.g. 'a' or 'aef
 
-            if(hasDrawCount) {
                 for (let index = 0; index < siteDeckSymbols.length; index++) {
-                    if(siteDeckSymbols[index] == combinationsArray[combinationsArrayIndex][selectedCombinationIndex]) {
-                        // If there is a direct match, do nothing; we already have this combination set
-                    }
-
+                    // If there is a direct match, do nothing; we already have this combination set
+                    if(siteDeckSymbols[index] == combinationsArray[combinationsArrayIndex][selectedCombinationIndex]) {}
+                    // If the symbol being compared is a wild card symbol
                     else if(combinationsArray[combinationsArrayIndex][selectedCombinationIndex] == MATCH_ANY) {
                         let newMatchedSet = combinationsArray[combinationsArrayIndex].slice(0);
+                        // Replace the symbol *before* we do checks, because it's a wildcard
                         newMatchedSet[selectedCombinationIndex] = siteDeckSymbols[index];
                         if(checkSymbolAmounts(newMatchedSet, siteDeckSymbols[index], decklistFrequencyMap[siteDeckSymbols[index]], false)) {
                             newMatchedSet.sort();
@@ -75,7 +111,7 @@ export function generateCombinations(initialCriteria, siteDeckSymbols, drawCount
                             }
                         }
                     }
-                    
+                    // If the original decklist symbol includes the compared symbol, then...
                     else if(siteDeckSymbols[index].includes(combinationsArray[combinationsArrayIndex][selectedCombinationIndex])) {
                         // create a new copy of the combination set
                         let newMatchedSet = combinationsArray[combinationsArrayIndex].slice(0);
@@ -93,33 +129,6 @@ export function generateCombinations(initialCriteria, siteDeckSymbols, drawCount
                         }
                     }
                 }
-            }
-
-            else {
-                for (let index = 0; index < reducedSiteDeckSymbols.length; index++) {
-                    if(reducedSiteDeckSymbols[index] == combinationsArray[combinationsArrayIndex][selectedCombinationIndex]) {
-                        // If there is a direct match, do nothing; we already have this combination set
-                    }
-                    else if(reducedSiteDeckSymbols[index].includes(combinationsArray[combinationsArrayIndex][selectedCombinationIndex])) {
-                        // create a new copy of the combination set
-                        let newMatchedSet = combinationsArray[combinationsArrayIndex].slice(0);
-                        // replace the value with the matched value only if there are fewer symbols of that type than the limit defined above
-                        if(checkSymbolAmounts(newMatchedSet, reducedSiteDeckSymbols[index], reducedDecklistFrequencyMap[reducedSiteDeckSymbols[index]], true)) {
-                            // Replace the item
-                            newMatchedSet[selectedCombinationIndex] = reducedSiteDeckSymbols[index];
-                            newMatchedSet.sort();
-                            // Check if the new set is unique
-                            if(!combinationSignatureArray.includes(newMatchedSet.join(","))) {
-                                combinationSignatureArray.push(newMatchedSet.join(","));
-                                combinationsArray.push(newMatchedSet);
-                                matchCount++;
-                            }
-                        }
-                    }
-                }
-            }
-
-
         }
     }
     // Remove the first entry seeded into the array that contains wildcard symbols
@@ -129,42 +138,6 @@ export function generateCombinations(initialCriteria, siteDeckSymbols, drawCount
     return combinationsArray;
 }
 
-
-/**
- * Reduces siteDeckSymbols' frequencies down to the maximum supported by the criteria
- * @param {Array<string>} criteria
- * @param {Array<string>} siteDeckSymbols
- * @returns {Array<string>} 
- */
-function reduceSiteDeckSymbols(criteria, criteriaFrequencyMap, siteDeckSymbols) {
-    // For each symbol type (a,e,f,w) reduce the frequency of that symbol down to the maximum number of symbols it matches with in the criteria array
-    // Example 1: 'a' array = [a,a,a,a,a] but criteria is [a,e,e,w]. Reduce the 'a' array down to [a], as there is only one possible match against the criteria
-    // Example 2: 'ae' array = [ae, ae, ae, ae]. Reduce down to [ae, ae, ae]
-    // TODO: Why am I doing this again?
-    const reducedSiteDeckSymbols = [];
-    for (let index = 0; index < siteDeckSymbols.length; index++) {
-        // exact matches, e.g. basic sites with one threshold
-        if(criteriaFrequencyMap.hasOwnProperty(siteDeckSymbols[index])) {
-            if(reducedSiteDeckSymbols.filter((letter) => siteDeckSymbols[index] == letter).length < criteriaFrequencyMap[siteDeckSymbols[index]]) {
-                reducedSiteDeckSymbols.push(siteDeckSymbols[index]);
-            }
-        } 
-        // multi-colored sites and others
-        else {
-            let includeCount = 0;
-            for (let j = 0; j < criteria.length; j++) {
-                if(siteDeckSymbols[index].includes(criteria[j])) {
-                    includeCount++;
-                }
-            }
-            if(includeCount > 0 && reducedSiteDeckSymbols.filter((letter) => letter == siteDeckSymbols[index]).length < includeCount) {
-                reducedSiteDeckSymbols.push(siteDeckSymbols[index]);
-            }
-        }
-    }
-    reducedSiteDeckSymbols.sort();
-    return reducedSiteDeckSymbols;
-}
 
 /**
  * Checks a possible match set's symbols to ensure they are below the ceiling defined in the decklistFrequecyMap
